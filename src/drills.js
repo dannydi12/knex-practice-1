b7fd9f15898055e373849ccca15554de36a01156
Evoxus <michael@michaelhploughman.com>
2020-02-11 22:09:13 -0700

    complete
    Co-authored-by: Daniel Di Venere <danieldivenere@gmail.com>

---
+++ src/drills.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: 'postgres://dunder_mifflin@localhost/knex-practice'
+})
+
+function searchByTerm(searchTerm) {
+  knexInstance
+    .select('name')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+// searchByTerm('to');
+
+function getAllItemsPaginated(pageNumber) {
+  const itemsPerPage = 6;
+  const offset = itemsPerPage * (pageNumber - 1)
+
+  knexInstance
+    .select('name', 'price')
+    .from('shopping_list')
+    .limit(itemsPerPage)
+    .offset(offset)
+    .then(result => console.log(result))
+
+}
+
+// getAllItemsPaginated(3);
+
+function getItemsAfterDate(daysAgo) {
+  knexInstance
+    .select('name', 'price', 'date_added')
+    .from('shopping_list')
+    .where('date_added',
+    '>',
+    knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
+    .then(result => {
+      console.log(result);
+    })
+}
+
+// getItemsAfterDate(5);
+
+function getCostByCategory() {
+
+  knexInstance
+    .select('category')
+    .sum({ "Total Cost": 'price' })
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log(result);
+    })
+}
+
+// SELECT
+//    category,
+//    SUM( price )
+// FROM
+//    shopping_list
+// GROUP BY
+//    category
+
+
+// SELECT COUNT(CustomerID), Country
+// FROM Customers
+// GROUP BY Country
+// ORDER BY COUNT(CustomerID) DESC;
+
+getCostByCategory();
