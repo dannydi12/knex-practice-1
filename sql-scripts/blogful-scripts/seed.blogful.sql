6b29d1c7d2c5c10ad8235e8aaab5ad727bd0769d
Evoxus <michael@michaelhploughman.com>
2020-02-13 20:35:23 -0700

    Setup for 2/13 assignment

---
+++ sql-scripts/blogful-scripts/seed.blogful.sql
@@ -0,0 +1,192 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    (
+    'How to be a better programmer',
+    now() - '10 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Challenged yet?',
+    now() - '13 days' :: INTERVAL,
+    'Raw denim coloring book artisan unicorn, bicycle rights trust fund PBR&B ennui waistcoat everyday carry. Flexitarian cloud bread roof party vice chicharrones, 
+    typewriter distillery cronut semiotics small batch +1 beard selvage. Deep v raclette tumblr trust fund, photo booth austin etsy viral vice literally 
+    cliche pitchfork. Pabst put a bird on it master cleanse, wolf organic tote bag DIY humblebrag iPhone meditation prism bitters distillery bespoke. Fanny pack 
+    asymmetrical XOXO roof party semiotics jianbing.'
+    ),
+    (
+    'Seeding SQL is tedius',
+    now() - '15 days' :: INTERVAL,
+    'Chartreuse authentic franzen, readymade vinyl taxidermy blue bottle gastropub knausgaard pitchfork deep v retro direct trade cold-pressed. Trust fund chia health 
+    goth, knausgaard semiotics gluten-free brooklyn actually williamsburg succulents twee farm-to-table food truck banh mi vape. Occupy twee brunch food truck, forage 
+    affogato slow-carb organic schlitz. XOXO thundercats vice crucifix dreamcatcher, listicle ugh heirloom bicycle rights. Af umami raclette paleo, gochujang 3 wolf moon 
+    VHS kogi coloring book. Unicorn af pok pok chartreuse tbh asymmetrical poutine woke bitters tote bag.'
+    ),
+    (
+    'THE END IS NIGH',
+    now() - '16 days' :: INTERVAL,
+    'An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. The other thing with Lorem Ipsum is that you have 
+    to take out its family. All of the words in Lorem Ipsum have flirted with me - consciously or unconsciously. That''s to be expected. Lorem Ipsum''s father was with 
+    Lee Harvey Oswald prior to Oswald''s being, you know, shot.'
+    ),
+    (
+    'Credibility is dead',
+    now() - '18 days' :: INTERVAL,
+    'I think the only card she has is the Lorem card. You''re telling the enemy exactly what you''re going to do. No wonder you''ve been fighting Lorem Ipsum your entire 
+    adult life. \n You could see there was text coming out of her eyes, text coming out of her wherever. This placeholder text is gonna be HUGE. You know, it really 
+    doesn''t matter what you write as long as you''ve got a young, and beautiful, piece of text.'
+    ),
+    (
+    'Lorem Ipsum Series End',
+    now() - '20 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 13',
+    now() - '21 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 12',
+    now() - '25 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 11',
+    now() - '27 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 10',
+    now() - '30 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 9b',
+    now() - '33 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 9a',
+    now() - '36 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 8',
+    now() - '37 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 7',
+    now() - '39 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 6',
+    now() - '41 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 5',
+    now() - '43 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 4',
+    now() - '45 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 3',
+    now() - '46 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 2',
+    now() - '49 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    ),
+    (
+    'Lorem Ipsum Series 1',
+    now() - '52 days' :: INTERVAL,
+    'Lorem ipsum dolor amet flexitarian brooklyn echo park, pabst activated charcoal twee shaman sartorial. Brooklyn narwhal raclette taxidermy try-hard. 
+        Vexillologist bitters plaid coloring book activated charcoal. Sustainable flexitarian brunch, art party skateboard jean shorts post-ironic kale chips cred retro. 
+        Godard kale chips pug fam banh mi viral bespoke man bun. Seitan squid copper mug farm-to-table. Gochujang tumeric raclette pabst jean shorts letterpress slow-carb 
+        hashtag polaroid mixtape iceland tumblr truffaut. \n Butcher crucifix letterpress keytar tilde cornhole godard prism. Austin meggings XOXO, vape adaptogen quinoa 
+        brooklyn. Art party church-key gochujang, raclette pitchfork tumeric organic beard cardigan. Coloring book 90'' s williamsburg chia authentic tofu hammock 
+        stumptown leggings fashion axe microdosing blue bottle retro truffaut.'
+    );
